1e70eb760db847e214d8a8c86fc88419ac7f95e8
gushiyuu123-glitch <gushiyuu123@icloud.com>
2026-07-25 21:29:58 +0900

    Allow main entry to host study reader preview

--- eslint.config.js
+++ eslint.config.js
@@ -18,4 +18,10 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  {
+    files: ['src/main.jsx'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
 ])
