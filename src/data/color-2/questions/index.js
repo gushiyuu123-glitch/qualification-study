bb803318cbcd8a35a51c28a91a9662ccb3c3165e
gushiyuu123-glitch <gushiyuu123@icloud.com>
2026-07-28 07:39:25 +0900

    色彩2級の問題源を過去問と分離接続

--- src/data/color-2/questions/index.js
+++ src/data/color-2/questions/index.js
@@ -1,3 +1,16 @@
 import { color2TextbookQuestions } from './textbook'
+import { color2PastExamQuestions } from './past-exams'
 
-export const color2Questions = [...color2TextbookQuestions]
+export { color2TextbookQuestions } from './textbook'
+export {
+  color2PastExam2025SummerQuestions,
+  color2PastExam2025WinterQuestions,
+  color2PastExamQuestions,
+  color2PastExamSources,
+  defineColor2PastExamQuestions,
+} from './past-exams'
+
+export const color2Questions = [
+  ...color2TextbookQuestions,
+  ...color2PastExamQuestions,
+]
